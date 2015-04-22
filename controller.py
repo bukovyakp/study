98abdcb91d237bbb7a2c3c01ce56806c3620cb1a
p.bukovyak <p.bukovyak@gmail.com>
2015-04-22 11:41:31 +0300

    add new file controller.py

---
+++ controller.py
@@ -0,0 +1,80 @@
+"""
+Controller.py
+Original version by Bruce Eckel
+Starts and manages solvers in separate processes for parallel processing.
+"""
+import sys
+from subprocess import Popen
+from twisted.spread import pb
+from twisted.internet import reactor, defer
+
+START_PORT = 5566
+MAX_PROCESSES = 2
+
+class Controller(object):
+
+    def broadcastCommand(self, remoteMethodName, arguments, nextStep, failureMessage):
+        print "controller.py: broadcasting..."
+        deferreds = [solver.callRemote(remoteMethodName, arguments) 
+                     for solver in self.solvers.values()]
+        print "controller.py: broadcasted"
+        reactor.callLater(3, self.checkStatus)
+
+        defer.DeferredList(deferreds, consumeErrors=True).addCallbacks(
+            nextStep, self.failed, errbackArgs=(failureMessage))
+    
+    def checkStatus(self):
+        print "controller.py: checkStatus"
+        for solver in self.solvers.values():
+            solver.callRemote("status").addCallbacks(
+                lambda r: sys.stdout.write(r + "\n"), self.failed, 
+                errbackArgs=("Status Check Failed"))
+
+    def __init__(self):
+        print "controller.py: init"
+        self.solvers = dict.fromkeys(
+            [("localhost", i) for i in range(START_PORT, START_PORT+MAX_PROCESSES)])
+        self.pids = [Popen(["python", "solver.py", str(port)]).pid
+                     for ip, port in self.solvers]
+        print "PIDS: ", self.pids
+        self.connected = False
+        reactor.callLater(1, self.connect)
+
+    def connect(self):
+        print "controller.py: connect"
+        connections = []
+        for address, port in self.solvers:
+            factory = pb.PBClientFactory()
+            reactor.connectTCP(address, port, factory)
+            connections.append(factory.getRootObject())
+        defer.DeferredList(connections, consumeErrors=True).addCallbacks(
+            self.storeConnections, self.failed, errbackArgs=("Failed to Connect"))
+
+        print "controller.py: starting parallel jobs"
+        self.start()
+
+
+    def start(self):
+        "controller.py: Begin the solving process"
+        if not self.connected:
+            return reactor.callLater(0.5, self.start)
+        self.broadcastCommand("step1", ("step 1"), self.step2, "Failed Step 1")
+
+    def step2(self, results):
+        print "controller.py: step 1 results:", results
+        self.broadcastCommand("step2", ("step 2"), self.step3, "Failed Step 2")
+
+    def collectResults(self, results):
+        print "controller.py: step 3 results:", results
+        self.terminate()
+        
+    def terminate(self):
+        print "controller.py: terminate"
+        for solver in self.solvers.values():
+            solver.callRemote("terminate").addErrback(self.failed, "Termination Failed")
+        reactor.callLater(1, reactor.stop)
+        return "Terminating remote solvers"
+
+if __name__ == "__main__":
+    controller = Controller()
+    reactor.run()
--- solver.py
+++ solver.py
@@ -13,16 +13,34 @@ class Solver(pb.Root):
         print "solver.py %s: solver init" % id
         self.id = id
 
+    def __str__(self): # String representation
+        return "Solver %s" % self.id
+
     def remote_initialize(self, initArg):
         return "%s initialized" % self
 
+    def step(self, arg):
+        print "solver.py %s: solver step" % self.id
+        "Simulate work and return result"
+        result = 0
+        for i in range(random.randint(1000000, 3000000)):
+            angle = math.radians(random.randint(0, 45))
+            result += math.tanh(angle)/math.cosh(angle)
+        return "%s, %s, result: %.2f" % (self, str(arg), result)
+
     # Alias methods, for demonstration version:
     remote_step1 = step
-
+    remote_step2 = step
+    remote_step3 = step
 
     def remote_status(self):
         print "solver.py %s: remote_status" % self.id
         return "%s operational" % self
+
+    def remote_terminate(self):
+        print "solver.py %s: remote_terminate" % self.id
+        reactor.callLater(0.5, reactor.stop)
+        return "%s terminating..." % self
 
 if __name__ == "__main__":
     port = int(sys.argv[1])
